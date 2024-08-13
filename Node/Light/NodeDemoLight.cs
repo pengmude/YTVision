fbf37fdeffe3349be0cf851a7cf0c09913281825
卿相 白衣 <2918546221@qq.com>
2024-08-13 08:53:16 +0800

    1

--- Node/Light/NodeDemoLight.cs
+++ Node/Light/NodeDemoLight.cs
@@ -5,27 +5,56 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using YTVisionPro.Node.Light;
+using YTVisionPro.Node.NodeDemo;
 
 namespace YTVisionPro.Node.NodeDemoLight
 {
-    public class NodeDemoLight : NodeBase, INode<NodeParamLight, NodeResultLight>
+    public class NodeDemoLight : NodeBase, INode<ParamFormLight, NodeParamLight, NodeResultLight>
     {
-        public override INodeParamForm ParamForm { get; set; }
+        /// <summary>
+        /// 创建一个指定名称的节点
+        /// </summary>
+        /// <param name="nodeText"></param>
+        public NodeDemoLight(string nodeText)
+        {
+            SetNodeText(nodeText);
+        }
 
+        /// <summary>
+        /// 参数设置窗口
+        /// </summary>
+        ParamFormLight INode<ParamFormLight, NodeParamLight, NodeResultLight>.ParamForm
+        {
+            get => (ParamFormLight)base.ParamForm;
+            set => base.ParamForm = value;
+        }
+
+        /// <summary>
+        /// 节点参数
+        /// </summary>
         public NodeParamLight Param { get; set; }
+
+        /// <summary>
+        /// 节点结果
+        /// </summary>
         public NodeResultLight Result { get; private set; }
 
+        /// <summary>
+        /// 节点运行
+        /// </summary>
+        /// <exception cref="NotImplementedException"></exception>
         public void Run()
         {
-            string SerialNumber = Param.SerialNumber;
-            
             Result = new NodeResultLight();
         }
 
-        protected override void ShowSettingsWindow()
+        /// <summary>
+        /// 给节点设置文本
+        /// </summary>
+        /// <param name="text"></param>
+        void INode<ParamFormLight, NodeParamLight, NodeResultLight>.SetNodeText(string text)
         {
-            if (base.Active)
-                ((ParamFormLight)ParamForm).ShowDialog();
+            base.SetNodeText(text);
         }
 
         //TODO: 目前 方案保存List<NodeBase>就能够兼容保存所有节点
